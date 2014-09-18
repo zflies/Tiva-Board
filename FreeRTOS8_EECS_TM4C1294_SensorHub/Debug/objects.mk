3d62decde8ab6271f634d0d05b4d1e1219c3a806
Zach Flies <zflies@ku.edu>
2014-09-18 08:53:40 -0500

    Delete Debug Folder

--- FreeRTOS8_EECS_TM4C1294_SensorHub/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/TI_CodeComposer/TivaWare/driverlib/ccs/Debug/driverlib.lib" -l"C:/TI_CodeComposer/TivaWare/sensorlib/ccs/Debug/sensorlib.lib"
-
